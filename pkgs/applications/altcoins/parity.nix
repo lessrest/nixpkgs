24f66aca603f5a0e364d20bc0d8d472ed0a1c7f2
Daniel Brockman <daniel@brockman.se>
2017-01-13 17:00:02 -0500

    parity: 1.4.5 -> 1.4.9

--- pkgs/applications/altcoins/parity.nix
+++ pkgs/applications/altcoins/parity.nix
@@ -4,17 +4,17 @@ with rustPlatform;
 
 buildRustPackage rec {
   name = "parity-${version}";
-  version = "1.4.5";
+  version = "1.4.9";
 
   src = fetchFromGitHub {
     owner = "ethcore";
     repo = "parity";
-    rev = "a028d04b01281812bb39136d96fa922343b978ee";
-    sha256 = "1ik9xsj5wqyfh1qm1wnnyalqxc4ww1xbr6cm4svpdkk2ml9fn7fr";
+    rev = "v${version}";
+    sha256 = "0r61sms436p6nyf19hpbx00va1cm2crc5y19hans68c13dbi6vyy";
   };
 
   buildInputs = [ perl ];
-  depsSha256 = "0865y7z9cnlh4pr1ix1p2s0hsjzz785cp5mr3mv35yr1ni42jxk4";
+  depsSha256 = "1bc9saxc0cp2y29v01qy8d9zq50gxbrsjmrvvzmci3p9ngdchay9";
 
   meta = {
     description = "Fast, light, robust Ethereum implementation";
